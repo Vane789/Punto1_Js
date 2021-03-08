764dbc84805e6b96b35c408020dbdaa24cb79fc5
Vane789 <78865050+Vane789@users.noreply.github.com>
2021-03-08 08:32:14 -0500

    Punto1

---
+++ Punto1/scripts/date.js
@@ -0,0 +1,4 @@
+/*Anlly Vanessa Cardona*/
+let f = new Date();
+document.write(f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear()+'<br>');
+document.write('Hora: '+f.getHours()+":"+f.getMinutes()+":"+f.getSeconds());
